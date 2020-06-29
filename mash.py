dca252b978ec96d2073c44ce389e2072855f958d
Connor Lagana <connor.lagana@gmail.com>
2020-06-29 18:58:25 -0400

    nothing

--- mash.py
+++ mash.py
@@ -2,7 +2,6 @@ from pydub import AudioSegment
 import wave
 import sys
 
-
 def mixNewSong(instrumental, firstV, secondV, vox, drop, chorus, name):
 
     instrumentalWav = AudioSegment.from_file(
@@ -24,5 +23,8 @@ def mixNewSong(instrumental, firstV, secondV, vox, drop, chorus, name):
 
 mixNewSong(sys.argv[1], sys.argv[2], sys.argv[3],
            sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+
+# sound = AudioSegment.from_file(sys.argv[7] + ".wav", format: = "wav")
+# play(sound)
 # mixNewSong("instrumental3", "firstV2", "secondV2",
 #            "vox3", "drop1", "chorus1", "titleOf20")
