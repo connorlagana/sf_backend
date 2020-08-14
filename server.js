2091e1d00ab9925f9f4fc5290ace2862fc2742cc
Connor Lagana <connor.lagana@gmail.com>
2020-08-13 21:23:32 -0400

    edited out python

--- server.js
+++ server.js
@@ -29,31 +29,31 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
 
-app.post("/newsong", (req, res) => {
-  var dataToSend;
-  // spawn new child process to call the python script
-  const python = spawn("python3", [
-    "mash.py",
-    req.body.instrumental,
-    req.body.firstV,
-    req.body.secondV,
-    req.body.vox,
-    req.body.drop,
-    req.body.chorus,
-    req.body.name,
-  ]);
-  // collect data from script
-  python.stdout.on("data", function (data) {
-    console.log("Pipe data from python script ...");
-    dataToSend = data.toString();
-  });
-  // in close event we are sure that stream from child process is closed
-  python.on("close", (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    res.send(dataToSend);
-  });
-});
+// app.post("/newsong", (req, res) => {
+//   var dataToSend;
+//   // spawn new child process to call the python script
+//   const python = spawn("python3", [
+//     "mash.py",
+//     req.body.instrumental,
+//     req.body.firstV,
+//     req.body.secondV,
+//     req.body.vox,
+//     req.body.drop,
+//     req.body.chorus,
+//     req.body.name,
+//   ]);
+//   // collect data from script
+//   python.stdout.on("data", function (data) {
+//     console.log("Pipe data from python script ...");
+//     dataToSend = data.toString();
+//   });
+//   // in close event we are sure that stream from child process is closed
+//   python.on("close", (code) => {
+//     console.log(`child process close all stdio with code ${code}`);
+//     // send data to browser
+//     res.send(dataToSend);
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`We're live at ${PORT}!`);
